634937f7b03ed10df4ff4a8af0e5042e57d5f865
Stephen M. Williams <smwilliams@tableausoftware.com>
2017-03-06 11:23:48 -0800

    First draft

---
+++ cleanuptool.py
@@ -0,0 +1,19 @@
+__author__ = 'smwilliams'
+
+# Install necessary libraries for Confluence API integration, CURL request, and reading/writing files
+
+# Define function for labeling pages
+
+# Define function for deleting pages
+
+# Define function for moving pages
+
+# Ask user which function they want to use
+
+# Ask user for credentials
+
+# Log into Confluence
+
+# Execute chosen function
+
+#
