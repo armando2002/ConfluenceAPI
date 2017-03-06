76bf53e21463faf2d5711aaa498fc39ae4157bcf
Stephen M. Williams <smwilliams@tableausoftware.com>
2017-03-06 14:29:20 -0800

    2nd try for labels

--- cleanuptool.py
+++ cleanuptool.py
@@ -5,33 +5,16 @@ import os
 import sys
 import json
 import getpass
+import requests
 from requests.auth import HTTPBasicAuth
 import csv
 
-# Initialize menu and ask user for choice
-print("Welcome to the Confluence API tool. \n")
-
-# Log into Confluence
-choiceloop = True
-while choiceloop:
-    choice = input("Choose one: (D)elete Pages, (M)ove Pages, or (L)abel Pages: ")
-    if choice.lower() == "d":
-        deletepaeges()
-        choiceloop = False
-    elif choice.lower() == "m":
-        movepages()
-        choiceloop = False
-    elif choice.lower() == "l":
-        labelpages()
-        choiceloop = False
-    else:
-        print("Please enter a valid choice.")
 
 # Ask user for credentials
 # QUESTION - Does Confluence have any SAML/Domain AUTH I could use instead?
 def login():
     username = input("Enter your Confluence username: ")
-    password = getpass.getpass("Enter your Confluence password: ")
+    password = getpass.getpass()
     basicauth = (username, password)
     return basicauth
 
@@ -56,3 +39,20 @@ def deletepaeges():
 # Define function for moving pages
 def movepages():
     pass
+
+# Initialize menu and ask user for choice
+print("Welcome to the Confluence API tool. \n")
+choiceloop = True
+while choiceloop:
+    choice = input("Choose one: (D)elete Pages, (M)ove Pages, or (L)abel Pages: ")
+    if choice.lower() == "d":
+        deletepaeges()
+        choiceloop = False
+    elif choice.lower() == "m":
+        movepages()
+        choiceloop = False
+    elif choice.lower() == "l":
+        labelpages()
+        choiceloop = False
+    else:
+        print("Please enter a valid choice.")
