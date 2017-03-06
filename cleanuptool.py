0ec78a410bc6b9f37c3ff8326b351a7dcfbe808a
Stephen M. Williams <smwilliams@tableausoftware.com>
2017-03-06 12:13:41 -0800

    Added initial code framework

--- cleanuptool.py
+++ cleanuptool.py
@@ -4,18 +4,31 @@ __author__ = 'smwilliams'
 import os
 import sys
 import json
+import getpass
+
+# Initialize menu and ask user for choice
+print "Welcome to the Confluence tool. \n"
+
+# Ask user for credentials
+username = rawinput("Enter your Confluence username: ")
+
+
+# Log into Confluence
+
+choice = raw_input("Choose one: (D)elete Pages, (M)ove Pages, or (L)abel Pages: ")
+if choice == "D":
+
 
 
 # Define function for labeling pages
+def labelpages():
+    pass
 
 # Define function for deleting pages
-
+def deletepaeges():
+    pass
 # Define function for moving pages
-
-# Ask user which function they want to use
-
-# Ask user for credentials
-
-# Log into Confluence
+def movepages():
+    pass
 
 # Execute chosen function
