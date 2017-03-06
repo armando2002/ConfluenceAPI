442e5ba4e5a52f2cd475a0479f147d90cdf0409b
Stephen M. Williams <smwilliams@tableausoftware.com>
2017-03-06 12:52:38 -0800

    Added login steps

--- cleanuptool.py
+++ cleanuptool.py
@@ -5,24 +5,38 @@ import os
 import sys
 import json
 import getpass
+import urllib.request
+import csv
 
 # Initialize menu and ask user for choice
-print "Welcome to the Confluence tool. \n"
+print("Welcome to the Confluence tool. \n")
 
 # Ask user for credentials
-username = rawinput("Enter your Confluence username: ")
-
+# QUESTION - Does Confluence have any SAML/Domain AUTH I could use instead?
+def login():
+    username = rawinput("Enter your Confluence username: ")
+    password = getpass.getpass("Enter your Confluence password: ")
+    basicauth = {'username': username, 'password': password}
 
 # Log into Confluence
-
 choice = raw_input("Choose one: (D)elete Pages, (M)ove Pages, or (L)abel Pages: ")
-if choice == "D":
-
-
+if choice == "D" or "d":
+    deletepaeges()
+elif choice == "M" or choice == "m":
+    movepages()
+elif choice == "L" or choice == "l":
+    labelpages()
+else:
+    print("Please enter a valid choice.")
+    # QUESTION - How do I loop back to the top if the user doesn't select a valid choice?
 
 # Define function for labeling pages
+# Initial version just has the user select one page, will iterate to reading from a file next
 def labelpages():
-    pass
+    url = 'https://mytableausandbox.tableaucorp.com/rest/api/audit?
+    print("\n Time to label some pages! \n")
+    login()
+
 
 # Define function for deleting pages
 def deletepaeges():
@@ -30,5 +44,3 @@ def deletepaeges():
 # Define function for moving pages
 def movepages():
     pass
-
-# Execute chosen function
