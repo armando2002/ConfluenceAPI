bf5150fe66bdc97866e4d0bef30779386c70d1d4
Stephen M. Williams <smwilliams@tableausoftware.com>
2017-03-09 13:13:35 -0800

    deletion and labeling are working

--- cleanuptool.py
+++ cleanuptool.py
@@ -1,18 +1,12 @@
 __author__ = 'smwilliams'
 
 # Install necessary libraries for Confluence API integration, CURL request, and reading/writing files
-import os
-import sys
-import json
 import getpass
 import requests
-from requests.auth import HTTPBasicAuth
 import csv
 #global variable for login
-basicauth = ()
 
 # Ask user for credentials
-# QUESTION - Does Confluence have any SAML/Domain AUTH I could use instead?
 def login():
     username = getpass.getuser()
     password = getpass.getpass()
@@ -20,23 +14,32 @@ def login():
     return basicauth
 
 # Define function for labeling pages
-# Initial version just has the user select one page, will iterate to reading from a file next
 def labelpages():
     print("\n Add a label to Confluence pages. \n")
     # calls the login function
     authorization = login()
-    pageid = input("\n Enter a page ID: ")
-    labelname = input("\n Enter a label: ")
+    labelname = input("\n Enter the label you'd like to add: ")
+    myFilePath = input('Enter the file path for your CSV of Page IDs (EG: C:\pageids.csv): ')
+    print('I will now read each Page ID in ' +myFilePath+ ' and add the ' + labelname + ' label to each page.')
     # Sets up the label addition per the Confluence API documentation
-    payload = {'prefix': 'global', 'name': labelname}
-    # sends an HTTP request to Confluence
-    url = 'https://mytableausandbox.tableaucorp.com/rest/api/content/' + pageid + '/' + "label"
-    r = requests.post(url, json=payload, auth=authorization, verify=False)
-    if r.status_code == 200:
-        print("\n Success!")
-    else:
-        print( "Error " + r.status_code)
+    with open(myFilePath, 'rt') as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            print('Adding {} label to page with PageID {}'.format(labelname, line[0]))
+            payload = {'prefix': 'global', 'name': labelname}
+            # sends an HTTP request to Confluence
+            url = 'https://mytableausandbox.tableaucorp.com/rest/api/content/{}/label'.format(line[0])
+            try:
+                r = requests.post(url, json=payload, auth=authorization, verify=False)
+                # checks response and informs user
+                if r.status_code == 200:
+                    print("\n Success!")
+                else:
+                    print( "Error " + r.status_code)
+            except Exception as e:
+                print('Error Occurred {}'.format(e))
     return None
+
 # Define function for deleting pages
 def deletepaeges():
     print("\n Delete Confluence pages. \n")
@@ -50,13 +53,16 @@ def deletepaeges():
         for line in reader:
             print('Requesting deletion of {}'.format(line[0]))
             url = 'https://mytableausandbox.tableaucorp.com/rest/api/content/{}'.format(line[0])
-            # deletes the page via HTTP request
-            r = requests.delete(url, auth=authorization, verify=False)
-            # checks response and informs user
-            if r.status_code == 200 or r.status_code == 204:
-                print("\n Succesfully deleted!")
-            else:
-                print( "Error " + str(r.status_code))
+            try:
+                # deletes the page via HTTP request
+                r = requests.delete(url, auth=authorization, verify=False)
+                # checks response and informs user
+                if r.status_code == 200 or r.status_code == 204:
+                    print("\n Succesfully deleted!")
+                else:
+                    print( "Error " + str(r.status_code))
+            except Exception as e:
+                print('Error Occurred {}'.format(e))
     return None
 
 # Initialize menu and ask user for choice
